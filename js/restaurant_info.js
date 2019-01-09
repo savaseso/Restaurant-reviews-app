d6e0792c1734577f6de8f8ba308236b0d9d950cf
savaseso <rigbence@gmail.com>
2019-01-09 20:55:38 +0100

    made index.html responsive for all devices

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2F2YXNlc28iLCJhIjoiY2pxcGU3bDlhMDBuNTQybGczNW04MDNpNiJ9.bf6lZtXIkXHSZ39gcjKjYQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
